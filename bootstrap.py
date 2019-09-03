74b68516f585bce5bc12c991cf6dfc9c600da155
Jacob Oscarson <jacob@414soft.com>
2019-09-03 15:28:06 +0200

    Sketched on an improved Python-based bootstrap.

--- bootstrap.py
+++ bootstrap.py
@@ -4,9 +4,27 @@
 import sys
 import os
 from os.path import expanduser as u
+import subprocess
 
-abspath = lambda base, *subdirs: os.path.join(os.path.abspath(u(base)), *subdirs)
+INSTALL = u('~/opt/plus')
+BASE = os.path.basename(__file__)
+
+abspath = lambda base, *subdirs: os.path.join(os.path.abspath(u(base)),
+                                              *subdirs)
 exists = lambda path: os.path.exists(u(path))
+
+
+def vpython(*subdirs):
+    # type: (*subdirs) -> None
+    "Does vpython"
+    return os.path.join(INSTALL, *(('py', ) + subdirs))
+
+
+def pip(*params):
+    # type: (*params) -> None
+    "Does pip"
+    subprocess.check_call((vpython('bin', 'pip'), *params))
+
 
 if 'defs':
     origin = os.path.dirname(os.path.abspath(__file__))
@@ -28,13 +46,27 @@ def bootstrap():
     # type: () -> None
     "Does bootstrap"
 
-    if exists('~/opt/plus/vgpy/bin/ipython'):
+    args = sys.argv[1:]
+
+    if exists(vpython('bin', 'python')):
         print('venv exist')
+        if args:
+            print('args')
+            os.execv(vpython('bin', 'python'), sys.argv[1:])
+        else:
+            print(vpython('bin', 'python'), ('-m plus.plus_exec', ))
+            os.execvpe(vpython('bin', 'python'), ('-m plus.plus_exec', ))
     else:
-        ensuredir('~/opt/plus')
-        import pdb
-        pdb.set_trace()
-        pass  # no virtualenv
+        import venv
+        eb = venv.EnvBuilder(system_site_packages=False,
+                             clear=False,
+                             symlinks=False,
+                             upgrade=False,
+                             with_pip=True,
+                             prompt=True)
+        eb.create(vpython())
+        pip('install', '-e', BASE)
+        os.execv(vpython('bin', 'python'), sys.argv[1:])
 
 
 if __name__ == '__main__':
