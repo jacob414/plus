228f5bd91544b3890f473daa14e7dcfd41ec5cf0
Jacob Oscarson <jacob@414soft.com>
2019-09-24 22:03:10 +0200

    Fixes incorrect parameter handling

--- plus/__main__.py
+++ plus/__main__.py
@@ -9,12 +9,15 @@ def cli_line(argvstring, params):
     # type: (str, List[str]) -> str
     "Does cli_line"
     first = argvstring.split(' ')[0]
-    return conf.shortcut_path(first, params)
+    return conf.shortcut_path(first, argvstring)
 
 
 if __name__ == '__main__':
     if '-p' in sys.argv:
         p_i_ = sys.argv.index('-p')
         shortcut = sys.argv[p_i_ + 1]
-        cmd = cli_line(shortcut, sys.argv[p_i_ + 1:])
+        try:
+            cmd = cli_line(shortcut, sys.argv[p_i_ + 1:])
+        except conf.ShortcutNotFound as exc:
+            print(f'+: shortcut for {str(exc)} not found')
         print(cmd)
