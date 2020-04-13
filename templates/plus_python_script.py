13f8a3164532b3060479b70a31476b830099b435
Jacob Oscarson <jacob@414soft.com>
2020-04-13 20:53:35 +0200

    Yapf

--- templates/plus_python_script.py
+++ templates/plus_python_script.py
@@ -9,11 +9,13 @@ import subprocess
 import sys
 from functools import partial
 
+
 @click.command()
 @click.pass_context
 def _xo_verb_xc_(ctx):
     "Docstring for {{verb}}"
     print(f"Implement the {{verb}} python script.")
-    
+
+
 if __name__ == '__main__':
     _xo_verb_xc_()
